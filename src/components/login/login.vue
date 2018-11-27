3a24f3317fe9681bb7be0ea5f81a9ab64c7ae9b4
张琳娜 <804870496@qq.com>
2018-11-27 10:46:49 +0800

    更改登录样式

---
+++ src/components/login/login.vue
@@ -0,0 +1,55 @@
+<template>
+        <div class="login-wrap">
+
+            <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+                <h2>用户登录</h2>
+                <el-form-item label="用户名">
+                    <el-input v-model="formData.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="formData.password"></el-input>
+                </el-form-item>
+                <el-button class="login-button" type="primary">登录</el-button>
+            </el-form>
+        </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           
+            formData: {
+                username: '',
+                password: ''
+            }
+        };
+    }
+}
+</script>
+<style>
+.login-wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.login-form {
+
+    width: 400px;
+    background-color: #fff;
+    padding: 30px;
+    border-radius: 10px
+}
+.login-button{
+    width: 100%;
+    margin-top: 20px;
+}
+el-input{
+    width: 100%;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../static/css/reset.css'
 
 Vue.use(Elementui)
 
